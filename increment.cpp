66982b49609bad4217ca4b83b152516883ca49c7
Hiroshi Nagata <hirosn@live.com>
2014-10-06 19:50:20 -0700

    Paste from CodePair QuickPad as comments

--- increment.cpp
+++ increment.cpp
@@ -18,6 +18,52 @@ using namespace std;
 //  Implementation
 //
 //------------------------------------------------------------------------------
+/*
+example: {1,2,3} represents 123 
+{1,2,3} -> {1,2,4}
+{3,4} -> {3,5}
+{9} -> {1,0}
+{-1,2,-3}
+{999,4,5}
+{1,2,MAXINT}
+nullptr
+size = -1
+size = 0
+size = MAXINT
+
+void increment(int*& array, int size) {
+    if (nullptr == array || size <= 0)
+        throw std::exception();
+
+    bool increment = true;
+    
+    for (int i = size - 1; increment && i >= 0; i--) {
+        if (array[i] < 0 || array[i] >= 10)
+            throw std::exception();
+            
+        if (++array[i] >= 10) {
+            array[i] = 0;
+        }
+        else {
+            increment = false;
+        }
+    }
+    
+    if (increment) {
+        if (size > MAXINT - 1)
+            throw std::exception();
+            
+        ++size;
+        int* newArray = new int[size];
+        newArray[0] = 1;
+        for (int i = 1; i < size; i++) {
+            newArray[i] = array[i - 1];
+        }
+        delete [] array;
+        array = newArray;
+    }
+}
+*/
 
 
 //------------------------------------------------------------------------------
