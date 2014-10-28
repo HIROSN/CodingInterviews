dc0493da9e132f9e2cd8248aa04857f2d5490a20
Hiroshi Nagata <hirosn@live.com>
2014-10-27 20:05:28 -0700

    Add files.

---
+++ subsets.cpp
@@ -0,0 +1,77 @@
+#include <SDKDDKVer.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+using namespace std;
+
+
+//------------------------------------------------------------------------------
+//
+//  Write a method to return all subsets of a set.
+//
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+//
+//  Implementation
+//
+//------------------------------------------------------------------------------
+vector<vector<int>> createSubsets(int array[], int size)
+{
+    if (nullptr == array)
+    {
+        throw exception();
+    }
+
+    vector<vector<int>> subsets;
+
+    for (int index = 0; index < size; index++)
+    {
+        if (0 == index)
+        {
+            subsets.push_back(vector<int>());
+        }
+
+        int n = subsets.size();
+
+        for (int j = 0; j < n; j++)
+        {
+            vector<int> a = subsets[j];
+            a.push_back(array[index]);
+            subsets.push_back(a);
+        }
+    }
+
+    return subsets;
+}
+
+
+//------------------------------------------------------------------------------
+//
+//  Demo execution
+//
+//------------------------------------------------------------------------------
+int _tmain(int argc, _TCHAR* argv[])
+{
+    int array[] = { 1, 2, 3, 4, 5 };
+    vector<vector<int>> subsets = createSubsets(array, ARRAYSIZE(array));
+
+    for (vector<int>::size_type i = 0; i < subsets.size(); i++)
+    {
+        vector<int> a = subsets[i];
+        cout << (i > 0 ? " " : "") << "[";
+
+        for (vector<int>::size_type j = 0; j < a.size(); j++)
+        {
+            cout << (j > 0 ? "," : "") << a[j];
+        }
+
+        cout << "]";
+    }
+
+    cout << endl;
+    return 0;
+}
